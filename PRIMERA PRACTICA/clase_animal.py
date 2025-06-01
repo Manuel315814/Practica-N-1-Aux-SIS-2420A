0f93178a580f9b6daec37c8615cae9d552bf026a
Manuel315814 <manuelbaltazar008@gmail.com>
2025-06-01 12:22:51 -0400

    Update clase_animal.py

--- PRIMERA PRACTICA/clase_animal.py
+++ PRIMERA PRACTICA/clase_animal.py
@@ -7,4 +7,16 @@ class Animal:
 
     def emite_sonido(self):
         print("Este animal realiza un sonido")
+# CREAMOS LAS TRES CLASES HIJAS
+class Perro(Animal):
+    def emite_sonido(self):
+        print("Guau Guau")
+
+class Gato(Animal):
+    def emite_sonido(self):
+        print("Miau")
+
+class Pajaro(Animal):
+    def emite_sonido(self):
+        print("Pío pío")
 
